035181602bb4553cd66ee358bb6fadc30d0aa91d
David Sanguano <dsanguano@advlatam.com>
2022-04-28 19:12:28 -0500

    proyecto base

---
+++ src/test/java/ec/com/comercio/ComercioApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.comercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
